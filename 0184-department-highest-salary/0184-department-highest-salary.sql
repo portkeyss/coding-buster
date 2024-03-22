82c666073347f82ef0b94e9dc413446d7eefac1f
portkeyss <wudajing@gmail.com>
2024-03-22 16:14:48 -0500

    Time: 1875 ms (10.81%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select d.Name Department, e1.name Employee, e1.Salary
+from Employee e1, Department d
+where e1.DepartmentId = d.Id 
+ and (select count(e2.Salary)
+          from Employee e2
+           where e2.DepartmentId = e1.DepartmentId and e2.Salary > e1.Salary) = 0
