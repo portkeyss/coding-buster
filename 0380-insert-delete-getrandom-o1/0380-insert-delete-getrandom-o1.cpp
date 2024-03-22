7b090d345c1c9605011e805dcafb53dafa09c326
portkeyss <wudajing@gmail.com>
2024-03-22 18:57:30 -0500

    Time: 149 ms (78.47%), Space: 100.7 MB (48.22%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,44 @@
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if(indices.find(val) != indices.end()) return false;
+        nums.emplace_back(val);
+        indices[val] = nums.size() - 1;
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if(indices.find(val) == indices.end()) return false;
+        int val_idx = indices[val];
+        int last_num = nums.back();
+        nums[val_idx] = last_num;
+        indices[last_num] = val_idx;
+        nums.pop_back(); 
+        indices.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+    
+private:
+    vector<int> nums;
+    unordered_map<int, int> indices;
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
