d721da4fe152b27e594b1a064cc2e1981e2e9481
portkeyss <wudajing@gmail.com>
2024-03-21 22:52:22 -0500

    Time: 16 ms (56.00%), Space: 11.6 MB (75.94%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        j = len(nums) - 1
+        while i <= j:
+            if nums[i] == val:
+                nums[i] = nums[j]
+                j -= 1
+            else:
+                i += 1
+        return j + 1
+        
+        
