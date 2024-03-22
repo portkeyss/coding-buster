ae1c8bbd0a8156740900ddc8537b2a50c2907e5a
portkeyss <wudajing@gmail.com>
2024-03-22 16:42:24 -0500

    Time: 10 ms (87.76%), Space: 58.9 MB (40.50%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for(int i : nums) {
+            if(hs.contains(i)) return true;
+            hs.add(i);
+        }
+        return false;
+    }
+}
